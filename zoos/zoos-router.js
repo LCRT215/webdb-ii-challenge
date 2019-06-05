16226ba646bd39d2162bd61c157ad0bf11c1e4d2
Leslie Thompson <lcrthompson@gmail.com>
2019-06-04 20:41:50 -0700

    mvp

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -29,4 +29,38 @@ router.get("/", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+router.get("/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .first()
+    .then(zoo => {
+      if (zoo) {
+        res.status(200).json(zoo);
+      } else {
+        res.status(404).json({ message: "There's no zoo at that index" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  db("zoos")
+    .where({ id: req.params.id })
+    .del()
+    .then(zoo => res.status(200).json(zoo))
+    .catch(err => res.status(500).json(err));
+});
+
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+
+  db("zoos")
+    .where({ id: req.params.id })
+    .update(changes)
+    .then(zoo => res.status(200).json(zoo))
+    .catch(err => res.status(500).json(err));
+});
+
 module.exports = router;
